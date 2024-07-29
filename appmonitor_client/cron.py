d5846a58bc36572e7658a6e3d972ecd99ec54446
Jason Moore <jmoore@austwa.com>
2024-07-29 14:50:19 +0800

    Update cron.py

--- appmonitor_client/cron.py
+++ appmonitor_client/cron.py
@@ -13,7 +13,7 @@ log = logging.getLogger(__name__)
 
 class CronJobAppMonitorClient(django_cron.CronJobBase):
     """Cron Job for the Catalogue Scanner."""
-    RUN_ON_DAYS = [0,1,2,3,4,5]
+    RUN_ON_DAYS = [1,2,3,4,5]
     RUN_AT_TIMES = ['9:15']
     schedule = django_cron.Schedule(run_on_days=RUN_ON_DAYS,run_at_times=RUN_AT_TIMES)
     code = "appmonitor_client.appmonitor_check"
