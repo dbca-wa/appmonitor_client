03e4e40df21d19251c925af40c37b10f27d8cc38
Jason Moore <jason.moore@dbca.wa.gov.au>
2024-08-16 17:45:57 +0800

    Fix for run_on_days and run_weekly_on_days

--- appmonitor_client/cron.py
+++ appmonitor_client/cron.py
@@ -15,7 +15,14 @@ class CronJobAppMonitorClient(django_cron.CronJobBase):
     """Cron Job for the Catalogue Scanner."""
     RUN_ON_DAYS = [1,2,3,4,5]
     RUN_AT_TIMES = ['9:15']
-    schedule = django_cron.Schedule(run_weekly_on_days=RUN_ON_DAYS,run_at_times=RUN_AT_TIMES)
+    dc = django_cron.Schedule()
+    dc_items = dc.__dict__
+    schedule = None
+    if 'run_weekly_on_days' in dc_items:         
+        schedule = django_cron.Schedule(run_weekly_on_days=RUN_ON_DAYS,run_at_times=RUN_AT_TIMES)
+    else:
+        schedule = django_cron.Schedule(run_on_days=RUN_ON_DAYS,run_at_times=RUN_AT_TIMES)
+    
     code = "appmonitor_client.appmonitor_check"
 
     def do(self) -> None:
